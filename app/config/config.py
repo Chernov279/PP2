e79b11467fd96fadb2857d684b7ae834107f4573
Chernov279 <chernov111444@gmail.com>
2024-10-20 18:08:45 +0300

    Start project. Create config

---
+++ app/config/config.py
@@ -0,0 +1,14 @@
+# Add this directory file .env with datas below
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_ENGINE = os.getenv("DB_ENGINE")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+SECRET_KEY = os.getenv("SECRET_KEY")
